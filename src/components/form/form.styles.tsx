acf76450ae366c8825a67a16c8d629acc3f0dcbb
lemmyosborne <bozhenko.rodion@mail.ru>
2022-02-25 16:42:46 +0300

    fix: form

--- src/components/form/form.styles.tsx
+++ src/components/form/form.styles.tsx
@@ -1,20 +1,142 @@
-import { Link } from "react-router-dom"
-import styled from "styled-components/macro"
+import styled from "styled-components"
 
-export const Container = styled.div``
+export const Container = styled.div`
+    width: 350px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
 
-export const Base = styled.form``
+export const Wrapper = styled.div`
+    width: 100%;
+    background-color: white;
+    border: solid 1px #dddada;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 20px;
+    margin-bottom: 10px;
+`
 
-export const Logo = styled.img``
+export const Base = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`
 
-export const Input = styled.input``
+export const Logo = styled.img`
+    width: 50%;
+    margin: 3rem 0;
+`
 
-export const Button = styled.button``
+export const Input = styled.input`
+    width: 75%;
+    min-height: 40px;
+    margin-bottom: 10px;
+    border: solid 1px #e8e8e8;
+    background-color: #f6f6f6;
+    border-radius: 4px;
+    overflow: hidden;
+    padding: 9px 0 7px 8px;
+    text-overflow: ellipsis;
 
-export const Divider = styled.span``
+    &::placeholder {
+        color: #afacb7;
+    }
+`
 
-export const FacebookLogin = styled(Link)``
+export const Button = styled.button`
+    width: 75%;
+    background-color: #b2dffc;
+    color: white;
+`
 
-export const ForgetPassword = styled.span``
+export const Divider = styled.span`
+    display: flex;
+    align-items: center;
+    margin: 20px 0 30px;
+    width: 75%;
+    font-size: 12px;
+    font-weight: 600;
+    color: grey;
 
-export const LoginSignupRedirect = styled.div``
+    &::before {
+        content: "";
+        display: inline-block;
+        width: 40%;
+        height: 1px;
+        margin-right: 15px;
+        background-color: lightgray;
+    }
+    &::after {
+        content: "";
+        display: inline-block;
+        width: 40%;
+        height: 1px;
+        margin-left: 15px;
+        background-color: lightgray;
+    }
+`
+
+export const FacebookLogin = styled.a`
+    color: #5c5f97;
+    font-weight: 600;
+    font-size: 14px;
+    margin-bottom: 20px;
+
+    img {
+        width: 15px;
+        height: auto;
+        margin-right: 10px;
+    }
+`
+
+export const ForgetPassword = styled.span`
+    color: #5c5f97;
+    font-size: 12px;
+    cursor: pointer;
+`
+
+export const LoginSignupRedirect = styled.div`
+    width: 350px;
+    background-color: white;
+    border: solid 1px #dddada;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+    font-size: 14px;
+
+    a {
+        color: #00a0f7;
+        font-weight: 600;
+        margin-left: 5px;
+    }
+`
+
+export const Bottom = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    span {
+        font-size: 14px;
+        margin: 20px auto;
+        width: auto;
+    }
+
+    div {
+        display: flex;
+        justify-content: center;
+    }
+
+    img {
+        height: 40px;
+        cursor: pointer;
+        display: inline;
+
+        &:first-of-type {
+            margin-right: 10px;
+        }
+    }
+`
--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,7 +1,7 @@
 import React from "react"
-import { LinkProps } from "react-router-dom"
 import {
     Container,
+    Wrapper,
     Base,
     Logo,
     Input,
@@ -10,28 +10,36 @@ import {
     FacebookLogin,
     ForgetPassword,
     LoginSignupRedirect,
+    Bottom,
 } from "./form.styles"
 
 interface FormComposition extends React.FC {
+    Wrapper: React.FC<React.HTMLAttributes<HTMLDivElement>>
     Base: React.FC<React.FormHTMLAttributes<HTMLFormElement>>
     Logo: React.FC<React.ImgHTMLAttributes<HTMLImageElement>>
     Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>>
     Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>>
     Divider: React.FC<React.HTMLAttributes<HTMLSpanElement>>
-    FacebookLogin: React.FC<LinkProps>
+    FacebookLogin: React.FC<React.AnchorHTMLAttributes<HTMLAnchorElement>>
     ForgetPassword: React.FC<React.HTMLAttributes<HTMLSpanElement>>
     LoginSignupRedirect: React.FC<React.HTMLAttributes<HTMLDivElement>>
+    Bottom: React.FC<React.HTMLAttributes<HTMLDivElement>>
 }
 
 export const Form: FormComposition = ({ children, ...rest }) => {
     return <Container {...rest}>{children}</Container>
 }
 
+Form.Wrapper = function FormWrapper({ children, ...rest }) {
+    return <Wrapper {...rest}>{children}</Wrapper>
+}
+
 Form.Base = function FormBase({ children, ...rest }) {
     return <Base {...rest}>{children}</Base>
 }
-Form.Logo = function FormLogo({ children, ...rest }) {
-    return <Logo {...rest}>{children}</Logo>
+
+Form.Logo = function FormLogo({ ...rest }) {
+    return <Logo src="/images/misc/logo.png" {...rest} />
 }
 
 Form.Input = function FormInput({ ...rest }) {
@@ -42,21 +50,35 @@ Form.Button = function FormButton({ children, ...rest }) {
     return <Button {...rest}>{children}</Button>
 }
 
-Form.Divider = function FormDivider({ children, ...rest }) {
-    return <Divider {...rest}>{children}</Divider>
+Form.Divider = function FormDivider({ ...rest }) {
+    return <Divider {...rest}>OR</Divider>
 }
 
-Form.FacebookLogin = function FormFacebookLogin({ children, ...rest }) {
-    return <FacebookLogin {...rest}>{children}</FacebookLogin>
+Form.FacebookLogin = function FormFacebookLogin({ ...rest }) {
+    return (
+        <FacebookLogin {...rest}>
+            <img src="/images/icons/facebook.png" />
+            Log in with Facebook
+        </FacebookLogin>
+    )
 }
 
-Form.ForgetPassword = function FormForgetPassword({ children, ...rest }) {
-    return <ForgetPassword {...rest}>{children}</ForgetPassword>
+Form.ForgetPassword = function FormForgetPassword({ ...rest }) {
+    return <ForgetPassword {...rest}>Forgotten your password?</ForgetPassword>
 }
 
-Form.LoginSignupRedirect = function FormLoginSignupRedirect({
-    children,
-    ...rest
-}) {
+Form.LoginSignupRedirect = function FormLoginSignupRedirect({ children, ...rest }) {
     return <LoginSignupRedirect {...rest}>{children}</LoginSignupRedirect>
+}
+
+Form.Bottom = function FormBottom({ ...rest }) {
+    return (
+        <Bottom {...rest}>
+            <span>Get the app.</span>
+            <div>
+                <img src="images/icons/apple.png" />
+                <img src="images/icons/google-play.png" />
+            </div>
+        </Bottom>
+    )
 }
