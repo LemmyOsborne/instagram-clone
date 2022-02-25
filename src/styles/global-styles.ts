36aaa7f21b7fec07c879aee7464a97d22d775cec
lemmyosborne <bozhenko.rodion@mail.ru>
2022-02-25 14:16:08 +0300

    styles: create global style

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background: #fafafa;
+    font-family: "Inter", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", 
+    "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif";
+    font-size: 16px;
+  }
+ 
+  button {
+    border: 0;
+    outline: none;
+    display: inline-block;
+    padding: 12px 24px;
+    font-size: 14px;
+    border-radius: 4px;
+    margin-top: 5px;
+    cursor: pointer;
+  }
+
+  a {
+  color: inherit;
+  text-decoration: none;
+}
+
+  input {
+  outline: none;
+}
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+export { GlobalStyles }
