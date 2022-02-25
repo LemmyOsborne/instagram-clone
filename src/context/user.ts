339f41900b9307e44370cc46363ee02f01904068
lemmyosborne <bozhenko.rodion@mail.ru>
2022-02-26 02:58:57 +0300

    feat: add user context and interface
    this context have all user's data, which stored in firestore

---
+++ src/context/user.ts
@@ -0,0 +1,4 @@
+import { IUser } from "interfaces/interfaces"
+import { createContext } from "react"
+
+export const UserContext = createContext({} as IUser)
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+    username: string
+    fullname: string
+    emailAddress: string
+    following: string[]
+    followers: string[]
+    dateCreated: number
+}
